97c33389dc1da67d5b6025f40d650e2c5ed92739
ejaakml <eximindotong@yahoo.com>
2020-11-27 21:38:55 +0700

    555

---
+++ src/quiz/if2/pkg10119061/rheizaakmalr/QUIZIF210119061RheizaAkmalR.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quiz.if2.pkg10119061.rheizaakmalr;
+
+/**
+ *
+ * @author Rheiza
+ */
+public class QUIZIF210119061RheizaAkmalR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
